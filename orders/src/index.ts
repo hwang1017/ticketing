65017b2d1132b0064245ab2441ece306c06e983e
hwang1017 <hru_wang@me.com>
2021-01-20 02:03:41 -0800

    add startup message to order service

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -10,6 +10,8 @@ import { natsWrapper } from './nats-wrapper'
 
 
 const start = async () => {
+  console.log('Starting...')
+
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be define')
   }
